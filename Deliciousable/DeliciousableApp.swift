7237f751d49b59ed08f588170f7618f9c667f865
craig-a-swanson <cswanson02@me.com>
2025-01-10 17:16:30 -0600

    Initial Commit

---
+++ Deliciousable/DeliciousableApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliciousableApp.swift
+//  Deliciousable
+//
+//  Created by Craig Swanson on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DeliciousableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeliciousableTests/DeliciousableTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeliciousableTests.swift
+//  DeliciousableTests
+//
+//  Created by Craig Swanson on 1/10/25.
+//
+
+import Testing
+@testable import Deliciousable
+
+struct DeliciousableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
